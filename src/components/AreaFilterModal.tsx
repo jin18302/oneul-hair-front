550ef2f822ae8923362638d3cd7aa016236cd0cc
jin18302 <jin18302@naver.com>
2025-11-06 17:50:28 +0900

    fix: 조건문 수정 및 불필요한 리소스 제거

--- src/components/AreaFilterModal.tsx
+++ src/components/AreaFilterModal.tsx
@@ -9,12 +9,11 @@ export default function AreaChoicePopup({closeModal, setSelectArea}
 
     const[areaCode, setAreaCode] = useState<string | null>(null);//areaCode가 바뀔때마다 페이지는 리렌더링된다.
     const[list, setList] = useState<AddrRes[]>([]);
-    let isSelectedArea = false;
     // 화면에 렌더링할 지역리스트를 관리
 
     useEffect(() => {
 
-        if(areaCode == '0' && isSelectedArea || areaCode?.length == 5 ){
+        if(areaCode == '0' || areaCode?.length == 5 ){
 
         const area = list.find(a => a.cd == areaCode);//사용자가 마지막으로 선택한 지역코드와 일치하는 것을 찾아 fullAddr를 출력한다
         setSelectArea(area?.fullAddress ?? "");
@@ -32,7 +31,6 @@ export default function AreaChoicePopup({closeModal, setSelectArea}
 
     const clickHandler = (cd: string) => {setAreaCode(cd); console.log("areaCode 상태 설정:", areaCode);} //지역리스트중 요소를 클릭한다면 areaCode를 변경한다
     const cancelHandler = () => {setAreaCode(null); closeModal();};// 취소버튼을 누르면 areaCode를 리셋하고 창을닫는다.
-    const checkHandler = () => {isSelectedArea = true}
 
     return (
         <div className="overlay">
@@ -41,7 +39,6 @@ export default function AreaChoicePopup({closeModal, setSelectArea}
                     <div className="element" key={r.cd} onClick={() => clickHandler(r.cd)}>{r.address}</div>
                 ))}
                 <button onClick={cancelHandler}>취소</button>
-                <button onClick={checkHandler}>확인</button>
             </div>
         </div>
     );
