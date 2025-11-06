2fa532af3a3c7e5cc00d778052a148d03819de53
jin18302 <jin18302@naver.com>
2025-11-06 15:41:36 +0900

    useEffect훅 내부 지역선택 종료시 로직처리위한 조건분기 추가

--- src/components/AreaChoicePopup.tsx
+++ src/components/AreaChoicePopup.tsx
@@ -7,25 +7,32 @@ import "../styles/Modal.css"
 export default function AreaChoicePopup({closeModal, setSelectArea}
     :{closeModal:() => void; setSelectArea:(area: string) => void}) {
 
-    const[areaCode, setAreaCode] = useState<string | null>(null);
+    const[areaCode, setAreaCode] = useState<string | null>(null);//areaCode가 바뀔때마다 페이지는 리렌더링된다.
     const[list, setList] = useState<AddrRes[]>([]);
+    let isSelectedArea = false;
     // 화면에 렌더링할 지역리스트를 관리
 
     useEffect(() => {
+
+        if(areaCode == '0' || areaCode?.length == 5 && isSelectedArea){
+
+        const area = list.find(a => a.cd == areaCode);//사용자가 마지막으로 선택한 지역코드와 일치하는 것을 찾아 fullAddr를 출력한다
+        setSelectArea(area?.fullAddress ?? "");
+        closeModal();
+        return;
+        }
+
         (async () => {
             const { data } = await axiosInstance.get<AddrRes[]>('/address', {params: {code: areaCode}});
             setList(data);
+            console.log('setList', list)
         })();
-    }, [areaCode]); //사용자가 지역을 선택하면 코드를 바꿀것이고, 그 코드가 바뀔때마다 api를 요청하여 다음 단계의 지역리스트를 읽어온다.
 
+    }, [areaCode]); //페이지가 리렌더링될때마다 api를 불러 상태를 변경하고, 이후에 변경된 리스트를 리렌더링한다.
 
-    const clickHandler = (cd: string) =>{setAreaCode(cd)}
-    const cancelHandler = () => {setAreaCode(null); closeModal();};
-    const checkHandler = () => {
-        const area = list.find(a => a.cd == areaCode);
-        setSelectArea(area?.fullAddress ?? "");
-        closeModal();
-    }
+    const clickHandler = (cd: string) => {setAreaCode(cd); console.log("areaCode 상태 설정:", areaCode);} //지역리스트중 요소를 클릭한다면 areaCode를 변경한다
+    const cancelHandler = () => {setAreaCode(null); closeModal();};// 취소버튼을 누르면 areaCode를 리셋하고 창을닫는다.
+    const checkHandler = () => {isSelectedArea = true}
 
     return (
         <div className="overlay">
