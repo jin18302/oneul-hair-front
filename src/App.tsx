c59b18b3d78a821891b9c035e95f5d52e935d381
jin18302 <jin18302@naver.com>
2025-11-14 00:03:55 +0900

    feat: 인증/인가 페이지 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { Route, Routes } from "react-router";
 import Main from "./pages/Main";
 import RootLayout from "./layouts/RootLayout";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import Authendication from "./components/Authendication";
 
 
 export default function App() {
 
-    return(
+    return (
         <>
-        <Routes>
-            <Route element={<RootLayout/>}>
-              <Route path="/" element={<Main />}/>
-            </Route>
-        </Routes>
+            <Routes>
+                <Route element={<RootLayout />}>
+                    <Route path="/" element={<Main />} />
+                    <Route path="auth" element={<Authendication />}>
+                        <Route path="login" element={<Login />} />
+                        <Route path="signup" element={<SignUp />} />
+                    </Route>
+                </Route>
+
+
+
+            </Routes>
         </>
     )
 }
