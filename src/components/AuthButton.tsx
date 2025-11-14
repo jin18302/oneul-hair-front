b09b535a4fcd6bde0a5060fdf8774280cd3c9a65
jin18302 <jin18302@naver.com>
2025-11-14 18:27:56 +0900

    feat: 불필요한 상태제거 및 link 설정

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
-import Authendication from "./Authendication";
-import "../styles/AuthButton.css"
+import { Link } from "react-router";
+import "../styles/AuthButton.css";
 
-//TODO: 버튼 css 수정
 export default function AuthButton(){
-    const [isShowAuthPage, setIsShowAuthPage] = useState<boolean>(false);
-
     return(
-        <div>
-            <button id = "auth-button" onClick={() => setIsShowAuthPage(true)}>로그인/회원가입</button>
-            {isShowAuthPage && <Authendication />}
-        </div>
+        <Link to="/auth/login"><button id="auth-button">로그인/회원가입</button></Link>
     )
 }
-
-//TODO: auth-button 을 누르면 네비게이션으로 auth/login페이지로 이동시킨다.
--- src/components/Authendication.tsx
+++ src/components/Authendication.tsx
@@ -1,30 +1,24 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import "../styles/Authendication.css"
-import { useState } from "react";
 
 export default function Authendication() {
-
-    const [pageType, setPageType] = useState<string>("login");
-
-    //기본적으로 로그인과 회원가입버튼을 전부 가지고 있어야함
-    //회원가입페이지일때 로그인버튼을 누르면 로그인페이지를 호출 || 로그인페이지일때 회원가입버튼을 누르면 회원가입페이지를 호출
-    //반대일 경우 submit처리를 한다.
-    //기본으로 login페이지와 매치시키고, 타입이 signup이라면 회원가입페이지를 매치시킨다.
-
-
-    const signupHandler = () => {setPageType("signup");}
-    const loginHandler = () => {setPageType("login");}
-
-    //+페이지 이동 
 
     return (
         <>
             <div className="container">
-                <div className="page-button" onClick={signupHandler} >회원가입</div>
-                <div className="page-button" onClick={loginHandler} >로그인</div>
+
+                <Link to="/auth/signup">
+                    <div className="page-button">회원가입</div>
+                </Link>
+
+                <Link to="/auth/login">
+                    <div className="page-button">로그인</div>
+                </Link>
+
                 <div className="authendication-container">
                     <Outlet />
                 </div>
+
             </div>
         </>
     )
