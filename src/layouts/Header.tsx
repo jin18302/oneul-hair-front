a645f98f72c8319304d3297cf95e4b6338193e99
jin18302 <jin18302@naver.com>
2025-11-25 15:09:14 +0900

    refactor: search 핸들러 위치 변경 및 props 제거

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -3,12 +3,12 @@ import SearchArea from "../components/SearchArea"
 import "../styles/Header.css"
 import "../styles/Layout.css"
 
-export default function Header({searchHandler}:{searchHandler:() => void}){
+export default function Header(){
 
     return(
         <header>
         <h1>onuel hair</h1>
-        <SearchArea searchHandler = {searchHandler}/>
+        <SearchArea/>
         </header>
     )
 }
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,30 +1,17 @@
-import { useSearchConditionContext } from "../hooks/UseSearchCondition";
+import { Outlet } from "react-router";
 import "../styles/Layout.css";
-import type { ShopTag } from "../types/ShopTag";
-import Header from "./Header";
 import Article from "./Article";
 import Footer from "./Footer";
-import { Outlet, useNavigate } from "react-router";
+import Header from "./Header";
 
 export default function RootLayout() {
 
-    const navigate = useNavigate();
-    const { selectArea, selectTags } = useSearchConditionContext();
-
-    const searchHandler = () => {
-        const params = new URLSearchParams();
-
-        if (selectArea != "전체") { params.set("area", selectArea); }
-        if (selectTags.length != 0) { params.set("tagIdList", selectTags.map((t: ShopTag) => t.id).join(",")); }
-
-        const navRequest = { pathname: "/search", search: `?${params.toString()}` }
-        navigate(navRequest); //검색버튼을 클릭하면 파람을 포함한 url로 이동된것처럼 사용자에게 보여준다.
-    }
+  
 
     return (
         <>
             <div className="layout-container">
-                <Header searchHandler={searchHandler} />
+                <Header  />
                 <Article />
                 <Outlet />
                 <Footer />
