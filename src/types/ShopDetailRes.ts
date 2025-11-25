e747fbcd9a5ddcfaf68cd46aa1e605fa4c8249a5
jin18302 <jin18302@naver.com>
2025-11-25 16:07:53 +0900

    refactor: 레이아웃 폴더 구조 수정

---
+++ src/types/ShopDetailRes.ts
@@ -0,0 +1,17 @@
+
+export interface ShopDetailRes {
+    id: number;
+    name: string;
+    address: string;
+    phoneNumber: string;
+    openTime: string;        // LocalTime → 문자열(예: "10:00")
+    endTime: string;         // LocalTime → 문자열
+    introduction: string;
+    imageUrlList: string;
+    snsUriList: string;
+    shopTagList: string[];   // List<String> → string[]
+    shopStatus: string;      // ShopStatus enum → 우선 string으로 받기
+    createdAt: string;       // LocalDateTime → ISO 문자열(예: "2025-11-25T10:00:00")
+    updatedAt: string;
+    deletedAt: string | null;
+}
