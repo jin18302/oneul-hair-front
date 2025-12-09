cf57c98e63f0d254af31bd917a814a77a7af4def
jin18302 <jin18302@naver.com>
2025-12-09 17:00:57 +0900

    feat: 디자이너 디테일 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router";
+import DesignerDetail from "./components/DesignerDetail";
 import Init from "./components/Init";
 import ShopDetail from "./components/ShopDetail";
 import ShopListView from "./components/ShopListView";
 import Main from "./layouts/rootLayout/Main";
-import SearchConditionProvider from "./providers/SearchConditionProvider";
 import RootLayout from "./layouts/rootLayout/RootLayout";
+import SearchConditionProvider from "./providers/SearchConditionProvider";
 
 export default function App() {
 
@@ -16,13 +17,12 @@ export default function App() {
                         <Route element={<Main/>}>
                          <Route path = "/" element={<Init />}/>
                          <Route path = "search" element={<ShopListView />}/>
-                         <Route path="shop/detail/:shopId" element={<ShopDetail />} />
+                         <Route path="shops/detail/:shopId" element={<ShopDetail />} />
+                         <Route path="designers/:designerId" element={<DesignerDetail />}/>
                         </Route>
                     </Route>
             </Routes>
-
         </>
-
     )
 }
 
--- src/components/DesignerDetail.tsx
+++ src/components/DesignerDetail.tsx
@@ -1,10 +1,59 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { axiosInstance } from "../AxiosInstance";
+import type { DesignerDetail } from "../types/DesignerDetail";
+import Calendar from "./Calendar";
 
-export default function DesignerDetail(){
+export default function DesignerDetail() {
 
+    const { designerId } = useParams<{ designerId: string }>();
 
+    const detailInit = {
+        id: 0,
+        shopId: 0,
+        name: "",
+        profileImage: "",
+        introduction: "",
+        imageUrlList: [],
+        snsUrlList: []
+    }
 
-    return(
+    const [designerDetail, setDesignerDetail] = useState<DesignerDetail>(detailInit);
+    const [isButtonClick, setButtonClick] = useState<boolean>(false);
+    const [isLoding, setIsLoding] = useState<boolean>(true);
+
+    useEffect(() => {
+
+        const apiHandler = async () => {
+
+            console.log("designer-detail loding");
+            const response = await axiosInstance.get<DesignerDetail>(`/designers/${designerId}`);
+
+            setDesignerDetail(response.data);
+            setIsLoding(false);
+        }
+
+        apiHandler();
+
+    }, []);
+
+    const buttonHandler = () => { setButtonClick(true); };
+
+    if (isLoding) { return <div>로딩중입니다...</div> };
+
+    return (
         <>
+
+            <div className="designer-detail">
+
+            </div>
+            {!isLoding &&
+                <div> <div className="images"> profile-image</div>
+                    <p>{designerDetail?.introduction}</p>
+                    <button className="reservation-button" onClick={buttonHandler}>예약하기</button></div>
+            }
+
+            {isButtonClick && <Calendar designerId={designerDetail?.id} />}
         </>
     )
 }
