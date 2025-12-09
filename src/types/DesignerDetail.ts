f457c94f7147b6740dbc3021a557dd19af7a0c33
jin18302 <jin18302@naver.com>
2025-12-09 16:58:25 +0900

    feat: 디자이너 type 정의

---
+++ src/types/DesignerDetail.ts
@@ -0,0 +1,10 @@
+
+export interface DesignerDetail {
+    id: number;
+    shopId: number;
+    name: string;
+    profileImage: string;
+    introduction: string;
+    imageUrlList: string[];
+    snsUrlList: string[];
+}
