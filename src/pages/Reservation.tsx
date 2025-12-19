aecb7050eadb78845aa66245d06f08d5c1075fc5
jin18302 <jin18302@naver.com>
2025-12-19 18:50:35 +0900

    feat: -디자이너 별 timeSlot 렌더링 페이지 구현

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Calendar from "../components/Calendar";
+import DateTimeSlot from "../components/DateTimeSlot";
+import { useParams } from "react-router";
+import MenuView from "./MenuView";
 
 export default function Reservation(){
 
+    const { designerId } = useParams();
+    const [ date, setDate ] = useState<string>("");
+    const [ timeSlot, setTimeSlot ] = useState<string>();
+
     return(
         <>
-        <Calendar />
+        <Calendar setDate = {setDate} designerId={designerId}  />
+        { date && <DateTimeSlot designerId={designerId} date = {date} setTimeSlot = {setTimeSlot}/>}
+        { timeSlot && <MenuView designerId = {designerId}/> }
         </>
     )
 }
---
+++ src/types/TimeSlotRes.ts
@@ -0,0 +1,6 @@
+
+
+export interface TimeSlotRes {
+    time: string,
+    isReservable: boolean
+}
