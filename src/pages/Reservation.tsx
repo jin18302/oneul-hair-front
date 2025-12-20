79ad4f075ad71b8352ed57850a77f52627eef2e9
jin18302 <jin18302@naver.com>
2025-12-20 19:30:32 +0900

    feat: - reservation 서버 요청 및 기능 구현 완성

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -3,18 +3,38 @@ import Calendar from "../components/Calendar";
 import DateTimeSlot from "../components/DateTimeSlot";
 import { useParams } from "react-router";
 import MenuView from "./MenuView";
+import { axiosInstance } from "../AxiosInstance";
+import { HttpStatusCode } from "axios";
 
-export default function Reservation(){
+export default function Reservation() {
 
     const { designerId } = useParams();
-    const [ date, setDate ] = useState<string>("");
-    const [ timeSlot, setTimeSlot ] = useState<string>();
+    const [date, setDate] = useState<string>("");
+    const [timeSlot, setTimeSlot] = useState<string>();
+    const [menu, setMenu] = useState<number | null>(null);
 
-    return(
+    const reservationHandler = async() => {
+
+        const requestBody = {
+            serviceMenuId: menu,
+            date: date,
+            time: timeSlot
+        };
+
+        const response = await axiosInstance.post(`/designers/${designerId}/reservations`, requestBody);
+
+        if( response.status === HttpStatusCode.Created ){
+            console.log("예약 성공", response.data);
+            // TODO: 몰라 어케하지
+        }
+    }
+
+    return (
         <>
-        <Calendar setDate = {setDate} designerId={designerId}  />
-        { date && <DateTimeSlot designerId={designerId} date = {date} setTimeSlot = {setTimeSlot}/>}
-        { timeSlot && <MenuView designerId = {designerId}/> }
+            <Calendar setDate={setDate} designerId={designerId} />
+            {date && <DateTimeSlot designerId={designerId} date={date} setTimeSlot={setTimeSlot} />}
+            {timeSlot && <MenuView designerId={designerId} setMenu={setMenu} />}
+            {menu && <button onClick={reservationHandler}>예약하기</button>}
         </>
     )
 }
