19a082bc0a94958eab791940734776d66306b46e
jin18302 <jin18302@naver.com>
2025-12-21 14:12:51 +0900

    fix: auth관련 라우트 오류 fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import RootLayout from "./layouts/rootLayout/RootLayout";
 import SearchConditionProvider from "./providers/SearchConditionProvider";
 import Init from "./components/Init";
 import Reservation from "./pages/Reservation";
+import SignUp from "./pages/Signup";
+import Login from "./pages/Login";
+import Authendication from "./components/Authendication";
 
 export default function App() {
 
@@ -19,7 +22,15 @@ export default function App() {
                 <Route element={<SearchConditionProvider><RootLayout /></SearchConditionProvider>}>
                     <Route element={<Main />}>
                         <Route path="/" element={<Init />} />
+
+                        {/* 로그인, 회원가입 관련 */}
+                        <Route path = "auth" element={<Authendication />}>
+                            <Route path = "sign-up" element={<SignUp />}/>
+                            <Route path = "sign-in" element={<Login />}/>
+                        </Route>
+
                         <Route path="search" element={<ShopListView />} />
+
                         <Route path="shops/detail/:shopId" element={<ShopDetail />} />
                         <Route path="designers/:designerId" element={<DesignerDetail />} />
                         <Route path="designers/:designerId/reservations" element={<Reservation />} />
