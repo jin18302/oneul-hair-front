60d69e4bee08dc1c4322b6272161f909b48736a6
jin18302 <jin18302@naver.com>
2025-12-24 12:57:40 +0900

    fix: -axios base url 'auth'제거

--- src/AxiosInstance.ts
+++ src/AxiosInstance.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({baseURL: "http://localhost:8080/api/auth"});
+export const axiosInstance = axios.create({baseURL: "http://localhost:8080/api"});
