7484cbb4783fb4257fa23d39d896b38de1cff14b
jin18302 <jin18302@naver.com>
2025-12-27 13:30:20 +0900

    css: - tag 요소 className 설정

--- src/components/searchElement/tag/TagFilterButton.tsx
+++ src/components/searchElement/tag/TagFilterButton.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react"
+// import "../../../styles/FilterArea.css"
+import { useSearchConditionContext } from "../../../hooks/UseSearchCondition";
 import TagFilterModal from "./TagFilterModal";
-import "../styles/FilterArea.css"
-import { useSearchConditionContext } from "../hooks/UseSearchCondition";
-
 
 export default function TagFilterButton(){
 
+     console.log("TagFilterButton rendering");
+
     const [isShowTagModal, setIsShowModal] = useState<boolean>(false);
+    const { selectTags } = useSearchConditionContext();
     const clickHandler = () => setIsShowModal(true);
-    const {selectTags} = useSearchConditionContext();
 
     return(
         <>
        
-              <button className= "filter-button" onClick={clickHandler}>태그 선택</button>
+              <button className= "filter-button" onClick={clickHandler}> 태그 선택 </button>
               {isShowTagModal && <TagFilterModal closeModal = {() => setIsShowModal(false)} />}
               {selectTags && selectTags.map(t => (<div key={t.id} className="seleted-element">{t.name}</div>))}
        
--- src/components/searchElement/tag/TagFilterModal.tsx
+++ src/components/searchElement/tag/TagFilterModal.tsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react"
-import { axiosInstance } from "../AxiosInstance"
-import type { ShopTag } from "../types/ShopTag";
-import { useSearchConditionContext } from "../hooks/UseSearchCondition";
+import { axiosInstance } from "../../../AxiosInstance";
+import { useSearchConditionContext } from "../../../hooks/UseSearchCondition";
+import type { ShopTag } from "../../../types/ShopTag";
+
 
 
 export default function SearchTagFilterModal({closeModal}:{closeModal: () => void;}){
+
+      console.log("SearchTagFilterModal rendering");
 
     const[tagList, setTagList] = useState<ShopTag[]>([]);
     const {selectTags, tagAdd} = useSearchConditionContext();
 
     useEffect(() => {
-        (   async () => {
-            const { data } = await axiosInstance.get<ShopTag[]>('/shop-tags');
+
+        const apiHandler =  async () => {
+            const { data } = await axiosInstance.get<ShopTag[]>('/auth/shop-tags');
             setTagList(data);
-        })();
+        };
+
+        apiHandler();
     },[]);
 
     const tagClickHandler = (s: ShopTag) => {
@@ -22,16 +28,14 @@ export default function SearchTagFilterModal({closeModal}:{closeModal: () => voi
             alert('태그는 최대 3개까지만 선택가능합니다.')
             return;
         }
-
         tagAdd(s);
-        console.log('tag 선택 후 tagList 상태:', selectTags)
     };
 
     return (
         <>
         <div className="overlay">
             <div className="modal">
-            {tagList.map(t => (<div key={t.id} onClick = {() => tagClickHandler(t)}>{t.name}</div>))}
+            {tagList.map(t => (<div className="element" key={t.id} onClick = {() => tagClickHandler(t)}>{t.name}</div>))}
             {/* 요소를 클릭한다면 요소의 클래스 이름을 추가하고, 리스트에 저장한다. */}
                 <button onClick={closeModal}>창닫기</button>
                 <button onClick={closeModal}>확인</button>
