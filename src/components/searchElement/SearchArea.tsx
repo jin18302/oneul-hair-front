9432aca055b0c182a1fc86916dab26b525699e24
jin18302 <jin18302@naver.com>
2025-12-29 01:34:22 +0900

    refactor: - searchHandler 위치 searchArea -> searchButton으로 변경

--- src/components/searchElement/SearchArea.tsx
+++ src/components/searchElement/SearchArea.tsx
@@ -1,40 +1,17 @@
-import { useNavigate } from "react-router";
 import "../../styles/SearchArea.css";
-import { useSearchConditionContext } from "../../hooks/UseSearchCondition";
-import type { ShopTag } from "../../types/ShopTag";
 import AreaFilterButton from "./area/AreaFllterButton";
 import SearchButton from "./SearchButton";
-import TagFilterButton from "./tag/TagFilterButton";
-// import "../styles/FilterArea.css";
-
-
+import TagFilterButton from "./tag/TagFilter";
 
 
 export default function SearchArea() {
 
      console.log("SearchArea rendering");
-
-      const navigate = useNavigate();
-    const { selectArea, selectTags } = useSearchConditionContext();
-
-    const searchHandler = () => {
-        console.log("search 핸들러 호출됨")
-        const params = new URLSearchParams();
-
-        if (selectArea != "전체") { params.set("area", selectArea); }
-        if (selectTags.length != 0) { params.set("tagIdList", selectTags.map((t: ShopTag) => t.id).join(",")); }
-
-        const navRequest = { pathname: "/search", search: `?${params.toString()}` }
-        navigate(navRequest); //검색버튼을 클릭하면 파람을 포함한 url로 이동된것처럼 사용자에게 보여준다.
-    }
-
-
     return (
             <div className="search-container">
-                <div className="search"></div>
                 <AreaFilterButton />
                 <TagFilterButton />
-               <SearchButton searchHandler={searchHandler}/>
+               <SearchButton/>
             </div>
     )
 }
--- src/components/searchElement/SearchButton.tsx
+++ src/components/searchElement/SearchButton.tsx
@@ -1,9 +1,28 @@
+import { useNavigate } from "react-router";
+import "../../styles/SearchArea.css"
+import { useSearchConditionContext } from "../../hooks/UseSearchCondition";
+import type { ShopTag } from "../../types/ShopTag";
+
+export default function SearchButton() {
+
+    console.log("SearchButton rendering");
+
+    const navigate = useNavigate();
+    const { selectArea, selectTags } = useSearchConditionContext();
+
+    const searchHandler = () => {
+
+        const params = new URLSearchParams();
+
+        if (selectArea != null) { params.set("area", selectArea); }
+        if (selectTags.length != 0) { params.set("tagIdList", selectTags.map((t: ShopTag) => t.id).join(",")); }
+
+        const navRequest = { pathname: "/search", search: `? ${params.toString()}` }
+        navigate(navRequest); //검색버튼을 클릭하면 파람을 포함한 url로 이동된것처럼 사용자에게 보여준다.
+    }
 
 
-export default function SearchButton({searchHandler}:{searchHandler:() => void}) {
-
-      console.log("SearchButton rendering");
     return (
-        <button id="search-button" onClick={searchHandler}>검색</button>
+        <button className="search-button" onClick={searchHandler}>검색</button>
     )
 }
