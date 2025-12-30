86d431b11eec4c454603fdce13ea6758f07c525e
jin18302 <jin18302@naver.com>
2025-12-31 02:10:53 +0900

    refactor:shopDetail useEffect관련 수정 -useEffct내 상태변경 메서드 호출 위치 변경 -shopId useEffect 의존 인자 설정 -shopDetail, designerList 상태 하나로 관리

--- src/components/ShopDetail.tsx
+++ src/components/ShopDetail.tsx
@@ -4,41 +4,41 @@ import { axiosInstance } from "../AxiosInstance";
 import type { ShopDetailRes } from "../types/ShopDetailRes";
 import type DesignerSummaryRes from "../types/DesignerSummaryRes";
 import "../styles/ShopDetail.css"
+import type { ShopDetailState } from "../types/ShopDetailState";
 
 export default function ShopDetail() {
 
-    const { shopId } = useParams<{ shopId: string }>();
+    console.log("ShopDetail rendering");
 
-    const [isLoding, setIsLoding] = useState<boolean>(true);
-    const [shopDetail, setShopDetail] = useState<ShopDetailRes>();
-    const [designerList, setDesignerList] = useState<DesignerSummaryRes[]>();
+    const { shopId } = useParams<{ shopId: string }>();
+    const navigate = useNavigate();
+    const dataInit = { shopDetail: null, designerList: [] };
+
+    const [ isLoding, setIsLoding ] = useState<boolean>(true); //서스펜서로 변경 고려
+    const [ shopDetailState, setShopDetailState ] = useState<ShopDetailState>(dataInit);
+    const { shopDetail, designerList } = shopDetailState;
 
     useEffect(() => {
 
-        const shopDetailApiHandler = async () => {
-            const response = await axiosInstance.get<ShopDetailRes>(`/shops/${shopId}`);
-            setShopDetail(response.data);
+        const apiHandler = async () => {
+
+            const shopDetailresponse = await axiosInstance.get<ShopDetailRes>(`/auth/shops/${shopId}`);
+            const designerListResponse = await axiosInstance.get<DesignerSummaryRes[]>(`/auth/shops/${shopId}/designers`);
+
+            const data = { shopDetail: shopDetailresponse.data, designerList: designerListResponse.data};
+
+            setShopDetailState(data);
+            setIsLoding(false);
         }
+        apiHandler();
 
-        const designerDataApiHandler = async () => {
-            console.log("디자이너 리스트 api 호출함");
-            const response = await axiosInstance.get<DesignerSummaryRes[]>(`/shops/${shopId}/designers`);
-            setDesignerList(response.data);
-            console.log(designerList);
-        }
+    }, [shopId]);
 
-        shopDetailApiHandler();
-        designerDataApiHandler();
-        setIsLoding(false);
+    const designerClickHandler = (d: DesignerSummaryRes) => { navigate(`/designers/${d.id}`); }
 
-    }, []);
+    if (isLoding) { return <div>로딩중입니다...</div>; }
 
-    const navigate = useNavigate();
-    const designerClickHandler = (d: DesignerSummaryRes) => {navigate(`/designers/${d.id}`);}
-
-    if (isLoding) { return <div>로딩중입니다...</div> ;}
-
-    return (<>
+    return ( <>
 
         {/* 
         TODO: 
@@ -67,14 +67,13 @@ export default function ShopDetail() {
             <div className="designer-list">
                 <h3>designers</h3>
                 {designerList && designerList.map(d =>
-                    <div className="designer" onClick={() => designerClickHandler(d)}>
+                    <div key={d.id} className="designer" onClick={() => designerClickHandler(d)}>
                         id:{d.id}<br />
                         image: {d.profileImage}<br />
                         name:{d.name}<br />
                         introduce:{d.introduction}<br />
                     </div>
                 )}
-
             </div>
         </div>
 
