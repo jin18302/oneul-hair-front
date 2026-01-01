da0dc145f76029aaf41967533ef9d73f61029bc3
jin18302 <jin18302@naver.com>
2026-01-02 03:56:37 +0900

    refactor: -fullCalendar props 코드 정리

--- src/components/reservationElement/Calendar.tsx
+++ src/components/reservationElement/Calendar.tsx
@@ -2,19 +2,21 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import FullCalendar from "@fullcalendar/react";
 import "../../styles/Calendar.css";
 import { useEffect, useState } from 'react';
-import interactionPlugin from "@fullcalendar/interaction";
+import interactionPlugin, { type DateClickArg } from "@fullcalendar/interaction";
 import { axiosInstance } from '../../AxiosInstance';
 import type { DayOffResponse } from '../../types/DayOffResponse';
 import React from 'react';
+import DateFommater from '../../hooks/DateFomatter';
+import type { DayCellContentArg } from '@fullcalendar/core/index.js';
 
 export function Calendar({ setDate, designerId }: { setDate: (date: string) => void, designerId: string | undefined }) {
 
   console.log("Calendar rendering");
 
   const dayOffInit = { closedDays: [] };
-  const [ dayOffList, setDayOffList ] = useState<DayOffResponse>(dayOffInit);
-  const [ month, setMonth ] = useState<number>(new Date().getMonth() + 1);
-  const [ isLoding, setIsLoding ] = useState<boolean>(true);
+  const [dayOffList, setDayOffList] = useState<DayOffResponse>(dayOffInit);
+  const [month, setMonth] = useState<number>(new Date().getMonth() + 1);
+  const [isLoding, setIsLoding] = useState<boolean>(true);
 
   useEffect(() => {
 
@@ -32,33 +34,43 @@ export function Calendar({ setDate, designerId }: { setDate: (date: string) => v
 
   }, [month]);
 
+  const isDayOff = (date: string) => {
+    return dayOffList.closedDays.includes(date);
+  }
+
+  const dateClickHandler = (arg: DateClickArg) => {
+    const isBefore = arg.date.getTime() < new Date().setHours(0, 0, 0, 0);
+
+    if (isBefore || isDayOff(arg.dateStr)) {
+      alert("해당 날짜는 예약 불가합니다.");
+      return;
+    }
+    setDate(arg.dateStr);
+  }
+
+  const classNameHandler = (arg: DayCellContentArg) => {
+    const date = DateFommater({ date: arg.date });
+
+    if (arg.isPast || isDayOff(date)) { return "disabled-day"; }
+    return "";
+
+  } //TODO: 클래스 또는 파일로 분리 고려
+
 
   if (isLoding) { return <div>loding...</div> }
 
   return (
 
-      <div className="calendar-container">
-        {!isLoding &&
-          <FullCalendar height={500} contentHeight={500}
-            plugins={[dayGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-
-            dateClick={(arg) => {
-              if (arg.date.getTime() <  new Date().setHours(0, 0, 0, 0) || dayOffList.closedDays.includes(arg.dateStr)){
-                alert("헤당 일자는 예약 불가합니다.");
-                return;
-              }
-              setDate(arg.dateStr);
-            }}
-
-            dayCellClassNames={(arg) => {
-                if (arg.date.getTime() <  new Date().setHours(0, 0, 0, 0)) { return "disabled-day"; }
-                if (dayOffList.closedDays.includes(arg.dateStr)) { return "fc_day_off"; }
-                return "";
-              }}
-          /> //full
-        }
-      </div>
+    <div className="calendar-container">
+      {!isLoding &&
+        <FullCalendar height={500} contentHeight={500}
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          dateClick={(arg) => dateClickHandler(arg)}
+          dayCellClassNames={(arg) => classNameHandler(arg)}
+        /> //fullCalendar
+      }
+    </div>
   )
 }
 
