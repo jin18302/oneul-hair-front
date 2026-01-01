0c8712fab574b70fa6691fa9fb499a5d9ee13bc5
jin18302 <jin18302@naver.com>
2026-01-01 21:18:15 +0900

    refactor: 비회원 예약기능 사용 시도시 로그인 페이지 리다이렉트 설정

--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
-import Calendar from "../components/Calendar";
-import DateTimeSlot from "../components/DateTimeSlot";
-import { useParams } from "react-router";
+import {useState } from "react";
+import Calendar from "../components/reservationElement/Calendar";
+import DateTimeSlot from "../components/reservationElement/DateTimeSlot";
+import { useNavigate, useParams } from "react-router";
 import MenuView from "../components/MenuView";
 import { axiosInstance } from "../AxiosInstance";
 import { HttpStatusCode } from "axios";
 
 export default function Reservation() {
 
+
+     console.log("Reservation rendering");
+
+     const navigator = useNavigate();
+
     const { designerId } = useParams();
-    const [date, setDate] = useState<string>("");
-    const [timeSlot, setTimeSlot] = useState<string>();
-    const [menu, setMenu] = useState<number | null>(null);
+    const [ date, setDate ] = useState<string>("");
+    const [ timeSlot, setTimeSlot ] = useState<string>();
+    const [ menu, setMenu ] = useState<number | null>(null);
+
+
+    //값과 함수들을 메모제이션
 
     const reservationHandler = async() => {
 
@@ -21,11 +29,19 @@ export default function Reservation() {
             time: timeSlot
         };
 
-        const response = await axiosInstance.post(`/designers/${designerId}/reservations`, requestBody);
+        const token = localStorage.getItem("token");
+
+        if(token == null){
+            alert("해당 서비스는 로그인 후 가능합니다.");
+            navigator("/auth/sign-in");
+        }
+
+        const response = await axiosInstance.post(`/designers/${designerId}/reservations`, 
+            requestBody,
+            { headers: { 'Authorization': token }});
 
         if( response.status === HttpStatusCode.Created ){
             console.log("예약 성공", response.data);
-            // TODO: 몰라 어케하지
         }
     }
 
