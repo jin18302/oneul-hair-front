4d21a0ec43de4df90dc588c595444c84aaf61997
jin18302 <jin18302@naver.com>
2026-01-03 19:11:48 +0900

    refactor: 로그인 정보관리 context -> zustand 변경

---
+++ src/contexts/userInfoStore.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import type { UserRes } from "../types/UserRes";
+
+export interface UserInfo{
+
+    isLoggedIn : boolean;
+    userName : string;
+    userRole : string;
+
+    setUserInfo: (u : UserRes) => void;
+    logOut : () => void;
+}
+
+export const userInfoStore = create<UserInfo>((set) => ({
+  
+    isLoggedIn : false,
+    userName : "",
+    userRole : "",
+
+    setUserInfo: (u : UserRes) => {set({isLoggedIn: true, userName: u.name, userRole: u.userRole})},
+    logOut : () => set({isLoggedIn: false, userName: "", userRole: ""})
+}));
