097828ff26b4cf1377f802edd8ed0fd2393de6b4
jin18302 <jin18302@naver.com>
2026-01-30 17:07:16 +0900

    fix: 파일 이름 오타 수정

--- src/utils/tokenmanager.ts
+++ src/utils/tokenmanager.ts
@@ -1,14 +1,18 @@
 
-let _accessToken = "";
+let _accessToken: string | undefined = undefined;
 
-export function setAccessToken(token : string){
+export function setAccessToken(token: string) {
     _accessToken = token;
 }
 
-export function getAccessToken(){
+export function getAccessToken() {
     return _accessToken;
 }
 
-export function resetToken(){
+export function resetToken() {
     _accessToken = "";
+}
+
+export function hasAccessToken(){
+    return _accessToken != undefined;
 }
