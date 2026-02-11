1e5e3a98a2d516d972f8d989f22e442dd31fdd67
jin18302 <jin18302@naver.com>
2026-02-11 12:05:09 +0900

    feat: 사용자 프로필 이미지 렌더링 설정

--- src/features/user/component/UserInfo.tsx
+++ src/features/user/component/UserInfo.tsx
@@ -2,29 +2,31 @@
 
 import { useNavigate } from "react-router";
 import { useGetUserInfoQuery } from "../hook/useUserQuery";
+import ImagePreview from "../../image/components/ImagePreview";
 
 export default function UserInfo() {
 
     console.log("UserInfo rendering");
-    
+
     const navigator = useNavigate();
-    const{data: userInfo} = useGetUserInfoQuery();
+    const { data: userInfo } = useGetUserInfoQuery();
 
     const infoUpdatePageHandler = () => { navigator('/users') }
     const pwUpdatePageHandler = () => { navigator('/password') }
     // const userDeleteHandler = () => {navigator('/password')}
 
     return (
-                <div className="user-info-container">
-                    <p className="user-info-element"> 이름 : {userInfo?.name}</p>
-                    <p className="user-info-element" > email : {userInfo?.email}</p>
-                    <p className="user-info-element"> userRole : {userInfo?.userRole}</p>
-                    <p className="user-info-element"> 성별 : {userInfo?.gender}</p>
-                    <p className="user-info-element" > 회원가입 일자 : {userInfo?.createdAt.substring(0, 10)}</p>
+        <div className="user-info-container">
+            {userInfo.profileImage && <ImagePreview image={userInfo?.profileImage} />}
+            <p className="user-info-element"> 이름 : {userInfo?.name}</p>
+            <p className="user-info-element" > email : {userInfo?.email}</p>
+            <p className="user-info-element"> userRole : {userInfo?.userRole}</p>
+            <p className="user-info-element"> 성별 : {userInfo?.gender}</p>
+            <p className="user-info-element" > 회원가입 일자 : {userInfo?.createdAt.substring(0, 10)}</p>
 
-                    <button onClick={() => infoUpdatePageHandler()}>정보 수정</button>
-                    <button onClick={() => pwUpdatePageHandler()}>비밀번호 수정</button>
-                    <button>회원 탈퇴</button>
-                </div>
+            <button onClick={() => infoUpdatePageHandler()}>정보 수정</button>
+            <button onClick={() => pwUpdatePageHandler()}>비밀번호 수정</button>
+            <button>회원 탈퇴</button>
+        </div>
     )
 }
