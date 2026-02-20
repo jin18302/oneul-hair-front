2eca2edc62515fedc22d6760bcd5d58d1ca91308
jin18302 <jin18302@naver.com>
2026-02-20 22:50:51 +0900

    feat: 이미지 프리뷰 기능 적용

--- src/features/shop/component/OwnerShopDetail.tsx
+++ src/features/shop/component/OwnerShopDetail.tsx
@@ -1,35 +1,38 @@
 import { useNavigate } from "react-router";
 import { useGetMyShopQuery } from "../hook/useShopQuery";
+import ImagePreview from "../../image/components/ImagePreview";
 
 
-export default function OwnerShopDetail(){
+export default function OwnerShopDetail() {
 
     console.log("ownerShopDetail rendering");
 
     const navigator = useNavigate();
-    const {data: shopDetail} = useGetMyShopQuery();
+    const { data: shopDetail } = useGetMyShopQuery();
 
-    return ( 
+    console.log("shopDetail", shopDetail)
+
+    return (
         <div className="shop-detail-container">
-            <div className="shop-images">imges</div>
-                <h2 className="info-element">{shopDetail?.name}</h2>
-                <p>{shopDetail.introduction}<br /></p>
-                <p>{shopDetail.address}<br /></p>
-                <p> {shopDetail?.phoneNumber}<br /></p>
-                <p> snsUriList: {shopDetail?.snsUriList}<br /></p>
-                <p>{shopDetail.imageUrlList}</p>
-                <p>{shopDetail.shopTagList}</p>
+            <ImagePreview image={shopDetail.mainImage}/>
+            <h2 className="info-element">{shopDetail.name}</h2>
+            <p>{shopDetail.introduction}<br /></p>
+            <p>{shopDetail.address}<br /></p>
+            <p> {shopDetail?.phoneNumber}<br /></p>
+            <p> snsUriList: {shopDetail?.snsUriList}<br /></p>
+            {shopDetail.shopTagIdSet.map(t =>
+                <div key={t.id}>{t.name}</div>)}
 
-                <p>
-                    운영상태: {shopDetail?.shopStatus}<br />
-                    운영시간 : {shopDetail?.openTime} ~ {shopDetail?.endTime}<br />
-                </p>
+            <p>
+                운영상태: {shopDetail?.shopStatus}<br />
+                운영시간 : {shopDetail?.openTime} ~ {shopDetail?.endTime}<br />
+            </p>
 
-                <p>{shopDetail.createdAt}</p>
-                <p>{shopDetail.updatedAt}</p>
-                <p>{shopDetail.deletedAt}</p>
+            <p>{shopDetail.createdAt}</p>
+            <p>{shopDetail.updatedAt}</p>
+            <p>{shopDetail.deletedAt}</p>
 
-                <button onClick={() => navigator(`/shops/${shopDetail?.id}/edit`)}>수정</button>
-            </div>
+            <button onClick={() => navigator(`/shops/${shopDetail?.id}/edit`)}>수정</button>
+        </div>
     )
 }
